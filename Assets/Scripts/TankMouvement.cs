c873914ae6dfaff6554666baf6f5a65eb94de067
gueguet57 <gverdinpol@students.eden.ualr.edu>
2019-08-02 17:53:09 -0500

    First commit

---
+++ Assets/Scripts/TankMouvement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankMouvement : MonoBehaviour
+{
+
+    public float speedMouvement = 1.0f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+        mouvePlayer();
+
+    }
+
+
+    public void mouvePlayer()
+    {
+
+
+
+        if (Input.GetKey("a"))
+        {
+            gameObject.transform.position += Vector3.left * speedMouvement * Time.deltaTime;
+        }
+
+
+        if (Input.GetKey("d"))
+        {
+            gameObject.transform.position += Vector3.right * speedMouvement * Time.deltaTime;
+        }
+
+
+        if (Input.GetKey("w"))
+        {
+            gameObject.transform.position += Vector3.forward * speedMouvement * Time.deltaTime;
+        }
+
+
+        if (Input.GetKey("s"))
+        {
+            gameObject.transform.position += Vector3.back * speedMouvement * Time.deltaTime;
+        }
+
+
+
+    }
+
+}
