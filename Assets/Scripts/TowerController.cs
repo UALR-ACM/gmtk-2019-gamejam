8341e89731096ddaadf6fd098047882a83ff6d54
gueguet57 <gverdinpol@students.eden.ualr.edu>
2019-08-03 13:25:20 -0500

    setupt tank

--- Assets/Scripts/TowerController.cs
+++ Assets/Scripts/TowerController.cs
@@ -6,33 +6,32 @@ public class TowerController : MonoBehaviour
 {
     Rigidbody rb;
     GameObject cam;
-    
+
     public GameObject altFireBullet;
     public float shotForce;
-    
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         Cursor.lockState = CursorLockMode.Confined;
         Cursor.visible = false;
         cam = transform.GetChild(0).gameObject;
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-     
+
         bool altFire = Input.GetButtonDown("Fire2");
 
         if (altFire)
         {
             GameObject cannonShell = Instantiate(altFireBullet, transform.position, transform.rotation);
-            Physics.IgnoreCollision(cannonShell.GetComponent<Collider>(), GetComponent<Collider>());
+            Physics.IgnoreCollision(cannonShell.GetComponent<Collider>(), transform.parent.gameObject.GetComponent<Collider>());
             cannonShell.GetComponent<Rigidbody>().AddForce(cam.transform.forward * shotForce);
-            
+
         }
 
     }
 }
-
