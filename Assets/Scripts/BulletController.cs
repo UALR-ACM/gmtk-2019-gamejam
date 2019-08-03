3fa6a2b8c90d1a469704385c4885a7d9cb3fac47
SWillis31 <sxwillis@ualr.edu>
2019-08-02 22:03:05 -0500

    Created alternate fire (artillery / cannon). Added a firerate to the primary weapon.

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{   
+    private void Update()
+    {
+        transform.Rotate(20, 0, 0);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //TODO: Enter damage numbers and add any explosion effects
+
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/TowerController.cs
+++ Assets/Scripts/TowerController.cs
@@ -6,35 +6,51 @@ public class TowerController : MonoBehaviour
 {
     Rigidbody rb;
     GameObject cam;
-
-    // Use this for initialization
+    public GameObject altFireBullet;
+    public float shotForce;
+    public float primaryFireTimer; //Time between primary fires (left-clicks)
+    private float primaryFire; //Determines whether or not the weapon can fire
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         Cursor.lockState = CursorLockMode.Confined;
         Cursor.visible = false;
         cam = transform.GetChild(0).gameObject;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        bool shoot = Input.GetButtonDown("Fire1");
+        bool shoot = Input.GetMouseButton(0);
         bool altFire = Input.GetButtonDown("Fire2");
 
-        if (shoot)
+        if (shoot && primaryFire >= primaryFireTimer)
         {
+            int layerMask = 1 << 2;
+            layerMask = ~layerMask;
             RaycastHit hit;
-            if (!Physics.Raycast(cam.transform.position, cam.transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+            if (!Physics.Raycast(cam.transform.position, cam.transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
             {
                 Debug.DrawRay(cam.transform.position, cam.transform.TransformDirection(Vector3.forward) * 1000, Color.red);
             }
             else
             {
-                Debug.DrawRay(cam.transform.position, cam.transform.TransformDirection(Vector3.forward) * hit.distance, Color.green);
+                Debug.DrawRay(cam.transform.position, cam.transform.TransformDirection(Vector3.forward) * hit.distance, Color.green, layerMask);
             }
+            primaryFire = 0f;
         }
+
+        if (altFire)
+        {
+            GameObject cannonShell = Instantiate(altFireBullet, transform.position, transform.rotation);
+            Physics.IgnoreCollision(cannonShell.GetComponent<Collider>(), GetComponent<Collider>());
+            cannonShell.GetComponent<Rigidbody>().AddForce(cam.transform.forward * shotForce);
+            
+        }
+
+        primaryFire += Time.deltaTime;
     }
 }
 
