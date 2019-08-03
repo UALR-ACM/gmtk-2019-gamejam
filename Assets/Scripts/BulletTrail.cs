35b61753cc7c8fd3c0e74938d43c129684685acd
SWillis31 <sxwillis@ualr.edu>
2019-08-03 11:38:40 -0500

    Added bullet trail effect for primary fire. Edited explosion FX.

---
+++ Assets/Scripts/BulletTrail.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletTrail : MonoBehaviour
+{
+    GameObject cam;
+    public LineRenderer trail;
+    public float trailWidth = 0.1f;
+    public float maxTrailLength = 5f;
+    public float primaryFireTimer; //Time between primary fires (left-clicks)
+    private float primaryFire; //Determines whether or not the weapon can fire
+    void Start()
+    {
+        Vector3[] trailInit = new Vector3[2] { Vector3.zero, Vector3.zero };
+        trail.SetPositions(trailInit);
+        trail.startWidth = trailWidth;
+        trail.endWidth = trailWidth;
+        cam = transform.GetChild(0).gameObject;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0) && primaryFire >= primaryFireTimer)
+        {
+            int layerMask = 1 << 2;
+            layerMask = ~layerMask;
+            RaycastHit hit;
+            Vector3 barrelExit = transform.position;
+            barrelExit.y -= 0.5f;
+            if (!Physics.Raycast(cam.transform.position, cam.transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask)) {
+                //Do damage stuff here
+            }
+
+            ShowTrail(barrelExit, cam.transform.forward, maxTrailLength);
+            trail.enabled = true;
+            primaryFire = 0f;
+        }
+        else
+        {
+            trail.enabled = false;
+        }
+        primaryFire += Time.deltaTime;
+    }
+
+    //Displays a line representing the path of the ray using the position of the tower, and the position of any object that the ray collides with
+    void ShowTrail(Vector3 targetPosition, Vector3 bulletDirection, float length)
+    {
+        Ray ray = new Ray(targetPosition, bulletDirection);
+        RaycastHit raycastHit;
+        Vector3 endPosition = targetPosition + (length * bulletDirection);
+
+        if(Physics.Raycast(ray, out raycastHit, length))
+        {
+            endPosition = raycastHit.point;
+        }
+
+        trail.SetPosition(0, targetPosition);
+        trail.SetPosition(1, endPosition);
+    }
+}
--- Assets/Scripts/TowerController.cs
+++ Assets/Scripts/TowerController.cs
@@ -8,10 +8,8 @@ public class TowerController : MonoBehaviour
     GameObject cam;
     
     public GameObject altFireBullet;
-    public GameObject regBullet;
     public float shotForce;
-    public float primaryFireTimer; //Time between primary fires (left-clicks)
-    private float primaryFire; //Determines whether or not the weapon can fire
+    
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -24,45 +22,8 @@ public class TowerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-        bool shoot = Input.GetMouseButton(0);
+     
         bool altFire = Input.GetButtonDown("Fire2");
-
-        if (shoot && primaryFire >= primaryFireTimer)
-        {
-            //GameObject bullet = Instantiate(regBullet, transform.localPosition, transform.rotation);
-            int layerMask = 1 << 2;
-            layerMask = ~layerMask;
-            RaycastHit hit;
-
-            //-------------------------BROKEN---------------------------
-            //LineRenderer line = gameObject.AddComponent<LineRenderer>();
-            //line.startColor = Color.yellow;
-            //line.endColor = Color.yellow;
-            //line.startWidth = 10f;
-            //line.endWidth = 10f;
-            //line.useWorldSpace = true;
-            //line.SetPosition(0, transform.position);
-            //GameObject bullet = Instantiate(regBullet, transform.position, transform.rotation);
-            //----------------------------------------------------------
-
-
-            if (!Physics.Raycast(cam.transform.position, cam.transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
-            {
-                Debug.DrawRay(cam.transform.position, cam.transform.TransformDirection(Vector3.forward) * 1000, Color.red);
-                //bullet.transform.Translate(hit.point);
-                //line.SetPosition(1, hit.point);
-            }
-            else
-            {
-                Debug.DrawRay(cam.transform.position, cam.transform.TransformDirection(Vector3.forward) * hit.distance, Color.green, layerMask);
-                //bullet.transform.Translate(hit.point);
-                //line.SetPosition(1, new Vector3(transform.position.x, transform.position.y, transform.position.z + 1000));
-            }
-            
-            
-            primaryFire = 0f;
-        }
 
         if (altFire)
         {
@@ -72,7 +33,6 @@ public class TowerController : MonoBehaviour
             
         }
 
-        primaryFire += Time.deltaTime;
     }
 }
 
