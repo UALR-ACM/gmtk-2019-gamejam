d226119e899cbf2b4d05442ad6f2e12c4fbba7f3
stewrat <email@nicholasrstewart.com>
2019-08-03 16:48:19 -0500

    Added AI movement

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -4,7 +4,18 @@ using UnityEngine;
 
 public class EnemyBehaviour : MonoBehaviour {
 
+    private PathWaypoint currentWaypoint;
+
     private void Update() {
-        
+        //Lerp(Vector3, Vector3, float)
+        // distance/speed
+
+        Vector3 start = currentWaypoint.transform.position;
+        Vector3 end = currentWaypoint.GetNextOnPath().transform.position;
+        float t = (Vector3.Distance(transform.position, end) + Time.deltaTime * 5f) / Vector3.Distance(start, end);
+
+        transform.Translate(Vector3.Lerp(start, end, t)); //vec3
+
     }
+    
 }
