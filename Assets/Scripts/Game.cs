cbc693b8be57d07421ac1365a8db44978da29d48
Denver Ellis <dsellis@students.eden.ualr.edu>
2019-08-03 11:32:31 -0500

    Committer: Denver Ellis <dsellis@students.eden.ualr.edu> 	modified:   Assets/Scripts/Game.cs 	modified:   Assets/Scripts/GameBoard.cs 	deleted:    Assets/Scripts/GameTile.cs.meta 	deleted:    Assets/Scripts/GameTileContent.cs.meta 	deleted:    Assets/Scripts/GameTileContentFactory.cs.meta 	deleted:    Assets/Scripts/GameTileContentType.cs.meta 	Edited Game.cs and GameBoard.cs to remove the tile system. Code 	now compiles but functionality is missing again.

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -5,19 +5,8 @@ using System.Collections.Generic;
 public class Game : MonoBehaviour {
     [SerializeField] Vector2Int boardSize = new Vector2Int(50, 50);
     [SerializeField] GameBoard board = default;
-    [SerializeField] GameTileContentFactory tileContentFactory = default;
 
     Ray TouchRay => Camera.main.ScreenPointToRay(Input.mousePosition);
-
-    void HandleTouch() {
-        GameTile tile = board.GetTile(TouchRay);
-        if(tile != null) board.ToggleWall(tile);
-    }
-
-    void HandleAlternativeTouch() {
-        GameTile tile = board.GetTile(TouchRay);
-        if(tile != null) board.ToggleDestination(tile);
-    }
 
     private void OnValidate() {
         if(boardSize.x < 2) boardSize.x = 2;
@@ -25,14 +14,10 @@ public class Game : MonoBehaviour {
     }
 
     void Awake() {
-        board.Initialize(boardSize, tileContentFactory);
+        board.Initialize(boardSize);
     }
 
     void Update() {
-        if(Input.GetMouseButtonDown(0)) {
-            HandleTouch();
-        } else if(Input.GetMouseButtonDown(1)) {
-            HandleAlternativeTouch();
-        }
+
     }
 }
--- Assets/Scripts/GameBoard.cs
+++ Assets/Scripts/GameBoard.cs
@@ -4,106 +4,20 @@ using System.Collections.Generic;
 
 public class GameBoard : MonoBehaviour {
     [SerializeField] Transform ground = default;
-    [SerializeField] GameTile tilePrefab = default;
-
     Vector2Int size;
-    GameTile[] tiles;
-    Queue<GameTile> searchFrontier = new Queue<GameTile>();
-    GameTileContentFactory contentFactory;
+    Queue<PathWaypoint> searchFrontier = new Queue<PathWaypoint>();
 	
-    public GameTile GetTile (Ray ray) {
-		if (Physics.Raycast(ray, out RaycastHit hit)) {
-			int x = (int)(hit.point.x + size.x * 0.5f);
-			int y = (int)(hit.point.z + size.y * 0.5f);
-			if (x >= 0 && x < size.x && y >= 0 && y < size.y) {
-				return tiles[x + y * size.x];
-			}
-		}
+    public PathWaypoint GetWayPoint (Ray ray) {
 		return null;
 	}
 
-    public void ToggleWall(GameTile tile) {
-        if(tile.Content.Type == GameTileContentType.Wall) {
-            tile.Content = contentFactory.Get(GameTileContentType.Empty);
-            FindPaths();
-        } else if(tile.Content.Type == GameTileContentType.Empty) {
-            tile.Content = contentFactory.Get(GameTileContentType.Wall);
-            if(!FindPaths()) {
-                tile.Content = contentFactory.Get(GameTileContentType.Empty);
-                FindPaths();
-            }
-        }
+    void FindPaths() {
+        return;
     }
 
-    public void ToggleDestination(GameTile tile) {
-        if(tile.Content.Type == GameTileContentType.Destination) {
-            tile.Content = contentFactory.Get(GameTileContentType.Empty);
-            if(!FindPaths()) {
-                tile.Content = contentFactory.Get(GameTileContentType.Destination);
-                FindPaths();
-            }
-        } else {
-            tile.Content = contentFactory.Get(GameTileContentType.Destination);
-            FindPaths();
-        }
-    }
-
-    bool FindPaths() {
-        foreach (GameTile tile in tiles) {
-            if(tile.Content.Type == GameTileContentType.Destination) {
-                tile.BecomeDestination();
-                searchFrontier.Enqueue(tile);
-            } else tile.ClearPath();
-        }
-        if(searchFrontier.Count == 0) return false;
-
-        while(searchFrontier.Count > 0) {
-            GameTile tile = searchFrontier.Dequeue();
-            if(tile != null) {
-                if(tile.IsAlternative) {
-                    searchFrontier.Enqueue(tile.GrowPathNorth());
-                    searchFrontier.Enqueue(tile.GrowPathSouth());
-                    searchFrontier.Enqueue(tile.GrowPathEast());
-                    searchFrontier.Enqueue(tile.GrowPathWest());
-                } else {
-                    searchFrontier.Enqueue(tile.GrowPathWest());
-                    searchFrontier.Enqueue(tile.GrowPathEast());
-                    searchFrontier.Enqueue(tile.GrowPathSouth());
-                    searchFrontier.Enqueue(tile.GrowPathNorth());
-                }
-            }
-        }
-
-        foreach (GameTile tile in tiles) {
-            tile.ShowPath();
-        }
-        return true;
-    }
-
-    public void Initialize (Vector2Int size, GameTileContentFactory contentFactory) {
+    public void Initialize (Vector2Int size) {
         //Create board of x,y size
         this.size = size;
-        this.contentFactory = contentFactory;
         ground.localScale = new Vector3(size.x, size.y, 1f);
-
-        //Instantiate tiles and add them to array tiles
-        Vector2 offset = new Vector2((size.x - 1) * 0.5f, (size.y - 1) * 0.5f);
-        tiles = new GameTile[size.x * size.y];
-        for(int i = 0, y = 0; y < size.y; y++) {
-            for(int x = 0; x < size.x; x++, i++) {
-                GameTile tile = tiles[i] = Instantiate(tilePrefab);
-                tile.transform.SetParent(transform, false);
-                tile.transform.localPosition = new Vector3(x - offset.x, 0f, y - offset.y);
-
-                //make tiles easy to search by
-                if(x > 0) GameTile.MakeEastWestNeighbors(tile, tiles[i-1]);
-                if(y > 0) GameTile.MakeNorthSouthNeighbors(tile, tiles[i - size.x]);
-                tile.IsAlternative = (x & 1) == 0;
-                if((y & 1) == 0) tile.IsAlternative = !tile.IsAlternative;
-                tile.Content = contentFactory.Get(GameTileContentType.Empty);
-            }
-        }
-
-        ToggleDestination(tiles[tiles.Length / 2]);
     }
 }
