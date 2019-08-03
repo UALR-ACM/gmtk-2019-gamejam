79b115b6c97f2630521fd24a0781c40a084e925b
SWillis31 <sxwillis@ualr.edu>
2019-08-02 22:19:47 -0500

    Added Cannonshell prefab

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BulletController : MonoBehaviour
-{   
+{
+    //public GameObject explosionEffect;
     private void Update()
     {
         transform.Rotate(20, 0, 0);
