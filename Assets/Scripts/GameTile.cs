4c1c5655effbc89ce604955c054f9ad4922708c2
Denver Ellis <dsellis@students.eden.ualr.edu>
2019-08-02 20:18:33 -0500

    Committer: Denver Ellis <dsellis@students.eden.ualr.edu> 	modified:   Assets/Scripts/GameTile.cs 	Expanded on path finding

--- Assets/Scripts/GameTile.cs
+++ Assets/Scripts/GameTile.cs
@@ -6,7 +6,13 @@ public class GameTile : MonoBehaviour{
     [SerializeField] Transform arrow = default;
     GameTile north, east, south, west, nextOnPath;
     int distance;
-    
+
+    public bool HasPath => distance != int.MaxValue;
+    public GameTile GrowPathNorth() => GrowPathTo(north);
+    public GameTile GrowPathEast() => GrowPathTo(east);
+    public GameTile GrowPathSouth() => GrowPathTo(south);
+    public GameTile GrowPathWest() => GrowPathTo(west);
+
     // Makes tiles easy to search by horizontally
     public static void MakeEastWestNeighbors (GameTile east, GameTile west) {
         Debug.Assert( west.east == null && east.west == null, "Redfined Neighbors!");
@@ -19,5 +25,31 @@ public class GameTile : MonoBehaviour{
         Debug.Assert(south.north == null && north.south == null, "Redfined Neighbors!");
         south.north = north;
         north.south = south;
+    }
+
+    // clears the path of a tile
+    public void ClearPath() {
+        distance = int.MaxValue;
+        nextOnPath = null;
+    }
+
+    // makes the tile the destination
+    public void BecomeDestination() {
+        distance = 0;
+        nextOnPath = null;
+    }
+
+    private void GrowPathTo(GameTile neighbor) {
+        Debug.Assert(HasPath, "No Path!");
+        if(neighbor == null || neighbor.HasPath) return;
+        neighbor.distance = distance + 1;
+        neighbor.nextOnPath = this;
+    }
+
+    public GameTile GrowPathTo(GameTile neighbor) {
+        if(!HasPath || neighbor == null) return null;
+        neighbor.distance = distance + 1;
+        neighbor.nextOnPath = this;
+        return neighbor;
     }
 }
