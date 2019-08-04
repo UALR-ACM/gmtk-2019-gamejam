99e7cc416e4c6fd39349622c179ee0659328a208
SWillis31 <34135272+SWillis31@users.noreply.github.com>
2019-08-04 12:33:31 -0500

    Fixed orb collision and implemented cannon cooldown

--- Assets/Scripts/TowerScript/TankBombController.cs
+++ Assets/Scripts/TowerScript/TankBombController.cs
@@ -10,7 +10,8 @@ public class TankBombController : MonoBehaviour
 
     public GameObject altFireBullet;
     public float shotForce;
-
+    public float secondaryFireTimer = 5f;
+    private float secondaryFire = 5f;
     // origin and direction of bulltes
     public GameObject originBulletLaunch;
     public GameObject firstCam;
@@ -29,13 +30,13 @@ public class TankBombController : MonoBehaviour
     {
 
         bool altFire = Input.GetButtonDown("Fire2");
-
-        if (altFire)
+        secondaryFire += Time.deltaTime;
+        if (altFire && secondaryFire >= secondaryFireTimer)
         {
             GameObject cannonShell = Instantiate(altFireBullet, originBulletLaunch.transform.position, transform.rotation);
             cannonShell.GetComponent<Rigidbody>().AddForce(firstCam.transform.forward * shotForce);
             soundManager.GetComponent<SoundManager>().PlayCannonShot();
-
+            secondaryFire = 0f;
         }
 
     }
