69a6d9a8b68cfee697961f99bb36783a42bc8922
John Clements <ClementsJR@users.noreply.github.com>
2019-08-04 13:03:57 -0500

    Finished UI Components

--- Assets/Scripts/CavasSwitcher.cs
+++ Assets/Scripts/CavasSwitcher.cs
@@ -7,37 +7,26 @@ public class CavasSwitcher : MonoBehaviour {
 	public RectTransform InstructionPanel;
 	public RectTransform GameUIPanel;
 	public RectTransform GameOverPanel;
-	public RectTransform CreditsPanel;
 
 	private void Start() {
 		InstructionPanel.gameObject.SetActive(true);
 		GameUIPanel.gameObject.SetActive(false);
 		GameOverPanel.gameObject.SetActive(false);
-		CreditsPanel.gameObject.SetActive(false);
 	}
 
-	private void BeginPlay() {
+	public void BeginPlay() {
 		InstructionPanel.gameObject.SetActive(false);
 		GameUIPanel.gameObject.SetActive(true);
 		GameOverPanel.gameObject.SetActive(false);
-		CreditsPanel.gameObject.SetActive(false);
 	}
 
-	private void EndGame() {
+	public void EndGame() {
 		InstructionPanel.gameObject.SetActive(false);
 		GameUIPanel.gameObject.SetActive(false);
 		GameOverPanel.gameObject.SetActive(true);
-		CreditsPanel.gameObject.SetActive(false);
 	}
 
-	private void DisplayCredits() {
-		InstructionPanel.gameObject.SetActive(false);
-		GameUIPanel.gameObject.SetActive(false);
-		GameOverPanel.gameObject.SetActive(true);
-		CreditsPanel.gameObject.SetActive(false);
-	}
-
-	private void NewGame() {
+	public void NewGame() {
 		SceneManager.LoadScene(0);
 	}
 }
