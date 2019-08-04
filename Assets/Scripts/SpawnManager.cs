1d922a8a498ea4822577762069a7f001630e6d85
John Clements <ClementsJR@users.noreply.github.com>
2019-08-03 23:53:45 -0500

    Spawner Prefab created

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,24 +4,80 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour {
 
-	public PathWaypoint[] spawnLocations;
+	public PathWaypoint[] spawners;
 	public Transform[] enemyPrefabs;
 	public float baseSpawnRate;
 	public int baseSpawnNumber;
+	public float restTime;
 
-	private int waveNumber;
+	private int waveNumber = 0;
+	private float currentSpawnRate;
+	private int currentSpawnNumber;
 	private float timeRemaining;
+	private float timeToNextSpawn;
+	private float remainingRestTime;
+
+	private bool inSpawnMode;
 
 	private void Start() {
 		Random.InitState((int)Mathf.Ceil(Time.deltaTime * Time.time * 100));
+		inSpawnMode = false;
+		remainingRestTime = restTime;
 	}
 
 	private void Update() {
+		if(inSpawnMode) {
+			CheckSpawn();
+		} else {
+			CheckRestTime();
+		}
+	}
 
+	private void CheckSpawn() {
+		timeRemaining -= Time.deltaTime;
+		timeToNextSpawn -= Time.deltaTime;
+
+		if (timeToNextSpawn <= 0f) {
+			Spawn();
+			timeToNextSpawn += currentSpawnRate;
+		}
+
+		if(timeRemaining <= 0f) {
+			remainingRestTime = restTime;
+			inSpawnMode = false;
+		}
 	}
 
 	private void Spawn() {
-		int spawnerIdx = Random.Range(0, spawnLocations.Length - 1);
+		int spawnerIdx = Random.Range(0, spawners.Length - 1);
 		int prefabIdx = Random.Range(0, enemyPrefabs.Length - 1);
+
+		Transform enemy = Instantiate(enemyPrefabs[prefabIdx]);
+		GameObject enemyGO = enemy.gameObject;
+
+		EnemyBehaviour behaviour = enemyGO.GetComponent<EnemyBehaviour>();
+		EntityStats stats = enemyGO.GetComponent<EntityStats>();
+
+		behaviour.currentWaypoint = spawners[spawnerIdx];
+		stats.SetPowerLevel(waveNumber * 10);
+	}
+
+	private void CheckRestTime() {
+		remainingRestTime -= Time.deltaTime;
+
+		if(remainingRestTime <= 0f) {
+			inSpawnMode = true;
+			PrepareForNextWave();
+		}
+	}
+
+	private void PrepareForNextWave() {
+		waveNumber++;
+
+		currentSpawnRate = waveNumber * baseSpawnRate;
+		currentSpawnNumber = waveNumber * baseSpawnNumber;
+
+		timeRemaining = currentSpawnRate * currentSpawnNumber;
+		timeToNextSpawn = currentSpawnRate;
 	}
 }
