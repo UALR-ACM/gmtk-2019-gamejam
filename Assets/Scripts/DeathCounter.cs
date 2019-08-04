5ad194cf183d4d6a9b806da8a931d93498ff1ee5
stewrat <email@nicholasrstewart.com>
2019-08-03 23:19:09 -0500

    Create DeathCount Class

---
+++ Assets/Scripts/DeathCounter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathCounter : MonoBehaviour
+{
+    private int totalDeathCount;
+
+    public int getTotalDeathCount() {
+        return totalDeathCount;
+    }
+
+    public void incrementTotalDeathCount() {
+        totalDeathCount += 1;
+    }
+}
